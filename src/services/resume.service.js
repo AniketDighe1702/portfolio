e8ac388692b81354a920d1d8366088a3e3b2269f
Aniket Dighe <aniketdighe@Anikets-MacBook-Air.local>
2024-07-31 21:55:25 +0530

    Update resume download link

--- src/services/resume.service.js
+++ src/services/resume.service.js
@@ -1,19 +1,15 @@
-const resumeLink = 'https://drive.google.com/file/d/1IDn9pJlLzI_DDtHM36PMN7YVpdCdu8jp/view';
+const resumeLink =
+  "https://drive.google.com/file/d/1IDn9pJlLzI_DDtHM36PMN7YVpdCdu8jp/view";
 
-
-async function get()
-{
-    return resumeLink;
+async function get() {
+  return resumeLink;
 }
 
-async function update(newLink)
-{
-    resumeLink = newLink;
+async function update(newLink) {
+  resumeLink = newLink;
 }
-
-
 
 module.exports = {
-    get, 
-    update
-}
+  get,
+  update,
+};
